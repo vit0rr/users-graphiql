325832ae58ac381eb1a60b573486f00e421888a7
Vitor <vi.souza.almeida@protonmail.com>
2022-05-24 17:50:13 -0300

    Fetch users inside of some companies

--- schema/schema.js
+++ schema/schema.js
@@ -1,20 +1,28 @@
-import graphql, { GraphQLInt, GraphQLString, GraphQLSchema } from "graphql";
+import graphql, { GraphQLInt, GraphQLString, GraphQLSchema, GraphQLList } from "graphql";
 import axios from "axios";
 
 const { GraphQLObjectType } = graphql;
 
 const CompanyType = new GraphQLObjectType({
   name: "Company",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
     description: { type: GraphQLString },
-  },
+    users: {
+      type: new GraphQLList(UserType),
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:3000/companies/${parentValue.id}/users`)
+          .then((res) => res.data);
+      },
+    },
+  }),
 });
 
 const UserType = new GraphQLObjectType({
   name: "User",
-  fields: {
+  fields: () => ({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
@@ -26,7 +34,7 @@ const UserType = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
-  },
+  }),
 });
 
 const RootQuery = new GraphQLObjectType({
