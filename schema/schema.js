f896dab5dc2550d6902e45fc0e94672795143f99
Vitor <vi.souza.almeida@protonmail.com>
2022-05-25 15:51:34 -0300

    Add and remove user mutation

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,10 @@
-import graphql, { GraphQLInt, GraphQLString, GraphQLSchema, GraphQLList } from "graphql";
+import graphql, {
+  GraphQLInt,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+  GraphQLNonNull,
+} from "graphql";
 import axios from "axios";
 
 const { GraphQLObjectType } = graphql;
@@ -60,6 +66,38 @@ const RootQuery = new GraphQLObjectType({
     },
   },
 });
+
+const mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    addUser: {
+      type: UserType,
+      args: {
+        firstName: { type: new GraphQLNonNull(GraphQLString) },
+        age: { type: new GraphQLNonNull(GraphQLInt) },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, { firstName, age }) {
+        return axios
+          .post("http://localhost:3000/users", { firstName, age })
+          .then((res) => res.data);
+      },
+    },
+    deleteUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parentValue, { id }) {
+        return axios
+          .delete(`http://localhost:3000/users/${id}`)
+          .then((res) => res.data);
+      },
+    },
+  },
+});
+
 export default new GraphQLSchema({
   query: RootQuery,
+  mutation,
 });
