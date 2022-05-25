cdb40640077cff8a74445937242538702083c475
Vitor <vi.souza.almeida@protonmail.com>
2022-05-25 17:00:03 -0300

    Adding Edit mutation

--- schema/schema.js
+++ schema/schema.js
@@ -94,6 +94,20 @@ const mutation = new GraphQLObjectType({
           .then((res) => res.data);
       },
     },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString },
+      },
+      resolve(parentValue, args) {
+        return axios
+          .patch(`http://localhost:3000/users/${args.id}`, args)
+          .then((res) => res.data);
+      },
+    },
   },
 });
 
